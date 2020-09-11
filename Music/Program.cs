93875c86ec9315c3c75f65c944acbae2ad3ca7a3
Swerik <derswerik@gmail.com>
2020-09-11 12:39:48 +0200

    finish AlleMeineEntchen

--- Music/Program.cs
+++ Music/Program.cs
@@ -4,14 +4,44 @@ namespace Music
 {
     internal static class Program
     {
-        internal static void Main(string[] args)
+        internal static void Main(string[] args) { AlleMeineEntchen(); }
+
+        private static void AlleMeineEntchen()
         {
+            MusicLibrary.Music.Bpm = 180;
             MusicLibrary.Music.PlayNote(Note.C2, Duration.Quarter);
             MusicLibrary.Music.PlayNote(Note.D2, Duration.Quarter);
             MusicLibrary.Music.PlayNote(Note.E2, Duration.Quarter);
             MusicLibrary.Music.PlayNote(Note.F2, Duration.Quarter);
             MusicLibrary.Music.PlayNote(Note.G2, Duration.Quarter);
+            MusicLibrary.Music.Pause(Duration.Quarter);
             MusicLibrary.Music.PlayNote(Note.G2, Duration.Quarter);
+            MusicLibrary.Music.Pause(Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.G2, Duration.Quarter);
+            MusicLibrary.Music.Pause(Duration.Full);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.A2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.G2, Duration.Quarter);
+            MusicLibrary.Music.Pause(Duration.Full);
+            MusicLibrary.Music.PlayNote(Note.F2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.F2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.F2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.F2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.E2, Duration.Quarter);
+            MusicLibrary.Music.Pause(Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.E2, Duration.Quarter);
+            MusicLibrary.Music.Pause(Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.D2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.D2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.D2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.D2, Duration.Quarter);
+            MusicLibrary.Music.PlayNote(Note.C2, Duration.Quarter);
         }
     }
 }
--- MusicLibrary/Music.cs
+++ MusicLibrary/Music.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace MusicLibrary
 {
@@ -6,9 +7,19 @@ namespace MusicLibrary
     {
         public static int Bpm = 120;
         public static double Bps => (double) Bpm / 60;
+        public static void Pause(int duration) { Thread.Sleep(duration); }
+        public static void Pause(double duration) { Thread.Sleep((int) Math.Round(duration)); }
         public static void PlayNote(int frequency, int duration) { Console.Beep(frequency, duration); }
         public static void PlayNote(Note frequency, int duration) { Console.Beep((int) frequency, duration); }
-        public static void PlayNote(int frequency, double duration) { Console.Beep(frequency, (int) duration); }
-        public static void PlayNote(Note frequency, double duration) { Console.Beep((int) frequency, (int) duration); }
+
+        public static void PlayNote(int frequency, double duration)
+        {
+            Console.Beep(frequency, (int) Math.Round(duration));
+        }
+
+        public static void PlayNote(Note frequency, double duration)
+        {
+            Console.Beep((int) frequency, (int) Math.Round(duration));
+        }
     }
 }
