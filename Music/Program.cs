08cfe6c729535ccc74e74c3bd5d80ec6646f4161
Swerik <derswerik@gmail.com>
2020-09-11 13:37:24 +0200

    Proper Main

--- Music/Program.cs
+++ Music/Program.cs
@@ -1,10 +1,31 @@
-﻿using MusicLibrary;
+﻿using System;
+using System.Collections.Generic;
+using MusicLibrary;
 
 namespace Music
 {
     internal static class Program
     {
-        internal static void Main(string[] args) { AlleMeineEntchen(); }
+        internal static void Main(string[] args)
+        {
+            var songs = new List<Action> {AlleMeineEntchen, AlleMeineEntchen};
+            var input = '@';
+            while (input != '0')
+            {
+                Console.WriteLine("\n\n\n\n\n\n\n\n\n");
+                Console.WriteLine("Select a Song:");
+                Console.WriteLine("[0]\tQuit");
+                for (var i = 0; i < songs.Count; i++)
+                    Console.WriteLine($"[{i + 1}]\t{songs[i].Method.Name}");
+
+                input = Console.ReadKey().KeyChar;
+                var intInput = input - '0';
+                if (intInput <= 0 || intInput > songs.Count) continue;
+
+                Console.WriteLine("\nNow Playing: {0}", songs[intInput - 1].Method.Name);
+                songs[intInput - 1].Invoke();
+            }
+        }
 
         private static void AlleMeineEntchen()
         {
