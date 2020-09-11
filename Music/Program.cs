01c0d257edf28b2eb66bc29adb9bf3830394a61a
Swerik <derswerik@gmail.com>
2020-09-11 12:28:15 +0200

    add C0-C2

--- Music/Program.cs
+++ Music/Program.cs
@@ -1,18 +1,18 @@
-﻿using System;
-
-namespace Music
+﻿namespace Music
 {
     internal static class Program
     {
         internal static void Main(string[] args)
         {
-            Console.Beep(300, 400);
-            Console.Beep(400, 400);
-            Console.Beep(500, 400);
-            Console.Beep(37, 400);
-            Console.Beep(300, 400);
-            Console.Beep(400, 400);
-            Console.Beep(500, 400);
+            // Console.Beep(300, 400);
+            // Console.Beep(400, 400);
+            // Console.Beep(500, 400);
+            // Console.Beep(37, 400);
+            // Console.Beep(300, 400);
+            // Console.Beep(400, 400);
+            // Console.Beep(500, 400);
+            var scraper = new TableScraper();
+            scraper.Scrape();
         }
     }
 }
